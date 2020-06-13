7941571bd6ae7c6e1434270ddbc2f40c7d578727
Connor Roizman <connorjayr@gmail.com>
2020-06-13 00:12:25 -0400

    Add !gg_ears command

--- roadhog/roadhog.py
+++ roadhog/roadhog.py
@@ -2,10 +2,15 @@ import discord
 from discord.ext import commands as cmd
 
 import roadhog.command.crab as crab
+import roadhog.command.gg_ears as gg_ears
 import roadhog.command.stfu as stfu
 
 class Roadhog(cmd.Bot):
-    commands = [crab.crab, stfu.stfu]
+    commands = [
+        crab.crab,
+        stfu.stfu,
+        gg_ears.gg_ears,
+    ]
 
     def __init__(self):
         cmd.Bot.__init__(self, command_prefix='!')
